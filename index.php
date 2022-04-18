928ad6029252fbc49fed8abe3d19160783b4a42c
unknown <ugyen.jigme2018@gmail.com>
2022-04-19 02:25:41 +0600

    Done with landing

---
+++ index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="assets/img/logo.svg" type="image/x-icon">
+    <!-- Bootstrap CSS -->
+  <?php include('components/link.php') ?>
+  <title>Jigme's Hiver</title>
+
+  </head>
+  <body>
+    <header class="header-area">
+
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-bg p-3 ">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="assets/img/logo.svg" alt="" width="60" class="d-inline-block align-text-top">
+        </a>
+        
+        <button class="navbar-toggler toogle-butt navbar-bg " type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="yellow fa fa-bars"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse " id="navbarTogglerDemo01">
+        <div class="me-auto"></div>
+        <hr class="text-light ">
+          <ul class="nav navbar-nav p-3 mb-2  mb-lg-0 ">
+            <li class="nav-item ">
+              <a class="nav-link  active"  href="#">ABOUT</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">BLOG</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">PROJECTS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">CONTACT</a>
+            </li>
+            
+          </ul>
+        </div>
+        
+      </div>
+      
+    </nav>
+    
+   
+    </header>
+    
+  
+    <!--Main Section-->
+    <section class="welcome-container pb-5">
+   
+      <div class="container text-white welcome-container p-4 pt-5 consolas">
+          <h6 class="welcome-text">Wanna know more about Jigme's Hive – This is the hive where you'll find your answers</h6>
+          <br>
+          <p class="welcome-text">Hello there!👋 I am Ugyen Jigme Rangdrel(--Jigme). The goof behind Jigme's Hive. So I setup this blog to talk about all the cool stuff happening in the World and about my Me.</p>
+          <p class="welcome-text">
+          Solana has taken off and now impossible to cover everything happening, we recommend you join your local Solana telegram group to stay up to date for the last info. (ie: Solana Australia)
+          </p>
+          <br>
+          <a href="" class="btn border border-light text-light explore-butt">Contact Jigme</a>
+      </div>
+      
+    </section>
+    
+    <section class="welcome-container pb-5">
+   
+      <div class="container text-white welcome-container p-3 consolas">
+          
+          <div class="row">
+            <div class="col-lg-8 col-md-7 blog-list">
+            <div class="col-lg-6 border-bottom border-primary mb-4">
+              <h6 class="b-container-title">Jigme's Blog Posts</h6>
+             
+              </div>
+              <br>
+              <div class="row ">
+                <div class="col-lg-6 col-md-12 col-sm-12 mb-5">
+                  <div class="img">
+                    <a href="">
+                      <img src="assets/img/background-1.svg" class="img-fluid" alt="">
+                    </a>
+                  </div>
+                  <br>
+                  <div class="description">
+                    <h6 class="text-primary">Solana</h6>
+                    <h5><a href="" class="blog-link">Blockchain Australia Week Kicks Off!</a></h5>
+                    <p class="blog-short">It’s a big week in Oz with Blockchain Week kicking off! Big sessions from Sam…</p>
+                  </div>
+                  <br>
+                  <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6 mb-5">
+                      ugyenjigmerangdrel
+                      <p class=" blog-short">March 24, 2022</p>
+                    </div>
+                    <div class="col-lg-1"></div>
+                    <div class="col-lg-5 col-md-5 col-sm-5 mt-2">
+                    <a href="" class="form-control btn border border-light text-light explore-butt p-2">Read More</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-12 col-sm-12 mb-3">
+                  <div class="img">
+                    <a href="">
+                      <img src="assets/img/background-1.svg" class="img-fluid" alt="">
+                    </a>
+                  </div>
+                  <br>
+                  <div class="description">
+                    <h6 class="text-primary">Solana</h6>
+                    <h5><a href="" class="blog-link">Solana Breakpoint Conference Nov 2021</a></h5>
+                    <p class="blog-short">It’s a big week in Oz with Blockchain Week kicking off! Big sessions from Sam…</p>
+                  </div>
+                  <br>
+                  <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6">
+                     ugyenjigmerangdrel
+                      <p class=" blog-short">March 24, 2022</p>
+                    </div>
+                    <div class="col-lg-1"></div>
+                    <div class="col-lg-5 col-md-5 col-sm-5 mt-2">
+                    <a href="" class="form-control btn border border-light text-light explore-butt p-2">Read More</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-12 col-sm-12 mb-3">
+                  <div class="img">
+                    <a href="">
+                      <img src="assets/img/background-1.svg" class="img-fluid" alt="">
+                    </a>
+                  </div>
+                  <br>
+                  <div class="description">
+                    <h6 class="text-primary">Solana</h6>
+                    <h5><a href="" class="blog-link">Solana Breakpoint Conference Nov 2021</a></h5>
+                    <p class="blog-short">It’s a big week in Oz with Blockchain Week kicking off! Big sessions from Sam…</p>
+                  </div>
+                  <br>
+                  <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6">
+                     ugyenjigmerangdrel
+                      <p class=" blog-short">March 24, 2022</p>
+                    </div>
+                    <div class="col-lg-1"></div>
+                    <div class="col-lg-5 col-md-5 col-sm-5 mt-2">
+                    <a href="" class="form-control btn border border-light text-light explore-butt p-2">Read More</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 col-md-12 col-sm-12 mb-3">
+                  <div class="img">
+                    <a href="">
+                      <img src="assets/img/background-1.svg" class="img-fluid" alt="">
+                    </a>
+                  </div>
+                  <br>
+                  <div class="description">
+                    <h6 class="text-primary">Solana</h6>
+                    <h5><a href="" class="blog-link">Solana Breakpoint Conference Nov 2021</a></h5>
+                    <p class="blog-short">It’s a big week in Oz with Blockchain Week kicking off! Big sessions from Sam…</p>
+                  </div>
+                  <br>
+                  <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6">
+                     ugyenjigmerangdrel
+                      <p class=" blog-short">March 24, 2022</p>
+                    </div>
+                    <div class="col-lg-1"></div>
+                    <div class="col-lg-5 col-md-5 col-sm-5 mt-2">
+                    <a href="" class="form-control btn border border-light text-light explore-butt p-2">Read More</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-5  post-board ">
+              <div class="board ">
+              <h5>Recent Posts</h5>
+              <br>
+              <h6>Solana Breakpoint Conference Nov 2021</h6>
+              <hr>
+              <h6>Solana Breakpoint Conference Nov 2021</h6>
+              <hr>
+              <h6>Solana Breakpoint Conference Nov 2021</h6>
+              <hr>
+              </div>
+            </div>
+          </div>
+      </div>
+      <hr class="text-white">
+    </section>
+    <footer class="blog-container">
+      <div class="container  text-light blog-container">
+        <h4>Jigme's Hive</h4>
+        <p>Founder@Bhutan Breakthrough</p>
+        <p class="blog-short">Copyright@UgyenJigmeRangdrel2022</p>
+      </div>
+      <br>
+      
+    </footer>
+  </body>
+</html>
